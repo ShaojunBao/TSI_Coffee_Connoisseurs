5ae65abd9e90bb764a5d39d38f71c94b33a7d88e
Shaojun Bao <youngwolf111@gmail.com>
2023-08-16 14:55:47 -0400

    Add review

--- main_app/models.py
+++ main_app/models.py
@@ -11,25 +11,16 @@ from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
 
-RATING_CHOICES = [(1,'1'),
-                  (2,'2'),
-                  (3,'3'),
-                  (4,'4'),
-                  (5,'5')]
-
-RATING_CHOICES = [(1,'1'),
-                  (2,'2'),
-                  (3,'3'),
-                  (4,'4'),
-                  (5,'5')]
+RATING_CHOICES = [
+    (1, '1 - Poor'),
+    (2, '2 - Fair'),
+    (3, '3 - Good'),
+    (4, '4 - Very Good'),
+    (5, '5 - Excellent')
+]
 
 
 
-class User_review(models.Model):
-    user_rating = models.IntegerField(
-        validators=[MinValueValidator(0), MaxValueValidator(5)]
-    )
-    user_review = models.TextField(max_length=250)
 
 class Coffee(models.Model):
     brand = models.CharField(max_length=200)
@@ -47,5 +38,40 @@ class Coffee(models.Model):
       
     def get_absolute_url(self):
         return reverse('detail', kwargs={'coffee_id': self.id})
+    
+
+class User_review(models.Model):
+    user_rating = models.CharField(
+        choices = RATING_CHOICES,
+        default = RATING_CHOICES[0][0]
+    )
+    user_review = models.TextField(max_length=250)
+    
+    coffee = models.ForeignKey(Coffee, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.user_review
+
+
+    
+    # timestamp = models.DateTimeField(auto_now_add=True, null=True)
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField ('date published') 
+    def __str__(self):
+        return self.question_text
+    
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    user = models.ForeignKey(User, on_delete=models.SET_DEFAULT, default=1)
 
     
