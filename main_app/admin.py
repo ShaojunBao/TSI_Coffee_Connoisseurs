f8d2139e5ef49159387d85c6cf71143b252704fc
Shaojun Bao <youngwolf111@gmail.com>
2023-08-15 21:25:03 -0400

    reverse to the good one, fix the issue that cannot add coffee

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Coffee, User_review, Question
+from .models import Coffee
 
 
 # Register your models here.
 admin.site.register(Coffee)
-admin.site.register(Question)
+
 
--- main_app/models.py
+++ main_app/models.py
@@ -2,6 +2,8 @@ import datetime
 from django.db import models
 from django.urls import reverse
 from django.utils import timezone
+from django.contrib.auth.models import User
+
 
 RATING_CHOICES = [(1,'1'),
                   (2,'2'),
